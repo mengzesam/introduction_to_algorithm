bce1b245ccbde53da40fb891e5f95389dfdcf8ac
mengzesam <mengzesam@126.com>
2018-09-13 09:30:44 +0800

    Add files via upload

--- chapter2.py
+++ chapter2.py
@@ -1,34 +1,91 @@
+class INSERTSORT:
+    def Solution(self,A):
+        if(type(A)!=list):
+            return -1
+        n=len(A)        
+        if(n==0):
+            return -1
+        for i in range(1,n):
+            key=A[i]
+            j=i-1
+            while j>=0 and A[j]>key:
+                A[j+1]=A[j]
+                j=j-1
+            A[j+1]=key
+        return 0
+
+class MERGESORT:
+    def Merge(self,A,p,q,r):
+        if not (p<=q and q<r):
+            print "p,q,r must be p<=q<r"
+            return -1
+        sentinel=1e10 #as infinity
+        n1=q-p+1
+        n2=r-q
+        L=[0]*(n1+1)
+        R=[0]*(n2+1)
+        for i in range(n1):
+            L[i]=A[p+i]
+        for j in range(n2):
+            R[j]=A[q+1+j]
+        L[n1]=sentinel
+        R[n2]=sentinel
+        i=0
+        j=0
+        for k in range(p,r+1):
+            if(L[i]<=R[j]):
+                A[k]=L[i]
+                i+=1
+            else:
+                A[k]=R[j]
+                j+=1
+        return 0
+
+    def MergeSort(self,A,p,r):
+        if(p<r):
+            q=int((p+r)/2)
+            self.MergeSort(A,p,q)
+            self.MergeSort(A,q+1,r)
+            self.Merge(A,p,q,r)
+               
+    def Solution(self,A):
+        if(type(A)!=list):
+            return -1
+        n=len(A)        
+        if(n==0):
+            return -1
+        self.MergeSort(A,0,n-1)
+        return 0
+
 class BUBBLESORT:
     def Solution(self,A):
-        if (type(A)!=list):
+        if(type(A)!=list):
             return -1
-        n=len(A)
-        if (n==0):
+        n=len(A)     
+        if(n==0):
             return -1
         for i in range(n-1,0,-1):
-            for j in range(0,i):
-                if (A[j]>A[j+1]):
+            for j in range(i):
+                if(A[j]>A[j+1]):
                     tmp=A[j]
                     A[j]=A[j+1]
                     A[j+1]=tmp
         return 0
-    def Solution2(self,A):
-        if (type(A)!=list):
-            return -1
-        n=len(A)
-        if (n==0):
-            return -1
-        for i in range(n-1):
-            for j in range(n-1,i,-1):
-                if (A[j-1]>A[j]):
-                    tmp=A[j-1]
-                    A[j-1]=A[j]
-                    A[j]=tmp
-        return 0
+
+
 
 
 if __name__=='__main__':
-    bubble_sort=BUBBLESORT()
-    A=[16,14,10,8,7,9,3,2,4,1]
-    bubble_sort.Solution2(A)
+    insert_sort=INSERTSORT()
+    A=[5,2,4,1,6,1,7,3]
+    insert_sort.Solution(A)
     print A
+    merger_sort=MERGESORT()
+    A=[8,3,9,12,11,3,10,1,7,13,6,4,2,5,11]
+    merger_sort.Solution(A)
+    print A
+    A=[8,3,9,12,11,3,10,1,7,13,6,4,2,5,11]
+    bubble_sort=BUBBLESORT()
+    bubble_sort.Solution(A)
+    print A
+
--- chapter6.py
+++ chapter6.py
@@ -1,16 +1,16 @@
-class HEAPSORT:
+class MAXHEAP:
+    sentinel_minus=-1e10
     def __init__(self,A):
         self.length=0
-        self.status=False
-        if (type(A)!=list):
+        self.isBuild=False
+        if(type(A)!=list):
             return        
         self.length=len(A)
-        if (self.length==0):
+        if(self.length==0):
             return
-        self.A=[]
-        for val in A:
-            self.A.append(val)
-        self.status=True
+        self.A=A
+        # for val in A:
+        #     self.A.append(val)
         self.heap_size=self.length
 
     def Left(self,i):
@@ -26,13 +26,13 @@ class HEAPSORT:
         l=self.Left(i)
         r=self.Right(i)
         largest=0
-        if (l<self.heap_size and self.A[l]>self.A[i]):
+        if(l<self.heap_size and self.A[l]>self.A[i]):
             largest=l
         else:
             largest=i
         if(r<self.heap_size and self.A[r]>self.A[largest]):
             largest=r
-        if (largest!=i):
+        if(largest!=i):
             tmp=self.A[i]
             self.A[i]=self.A[largest]
             self.A[largest]=tmp
@@ -42,6 +42,7 @@ class HEAPSORT:
         mid=int(self.heap_size/2)
         for i in range(mid-1,-1,-1):
             self.maxHeapify(i)
+        self.isBuild=True
 
     def heapSort(self):
         self.buildMaxHeap()
@@ -52,12 +53,69 @@ class HEAPSORT:
             self.heap_size-=1
             self.maxHeapify(0)
         self.heap_size=self.length
+    
+    def heapMaximum(self):
+        """         max-priority queue maximum """
+        if(not self.isBuild):
+            self.buildMaxHeap()
+        return self.A[0]
+    
+    def heapExtractMax(self):
+        """         max-priority queue"""
+        if(self.heap_size<1):
+            print "error:heap underflow"
+            return self.sentinel_minus
+        if(not self.isBuild):
+            print "max heap is not built"
+            return self.sentinel_minus
+        heap_max=self.A[0]
+        self.A[0]=self.A[self.heap_size-1]
+        self.A[self.heap_size-1]=heap_max
+        self.heap_size-=1
+        self.maxHeapify(0)
+        return heap_max
+
+    def heapIncreaseKey(self,i,key):
+        if(not self.isBuild):
+            print "max heap is not built"
+            return -1
+        if(key<=self.A[i]):
+            print "new key is not larger than current key"
+            return -1
+        self.A[i]=key
+        while(i>0 and self.A[self.Parent(i)]<self.A[i]):
+            tmp=self.A[i]
+            self.A[i]=self.A[self.Parent(i)]
+            self.A[self.Parent(i)]=tmp
+            i=self.Parent(i)
+        return 0
+        
+    def maxHeapInsert(self,key):
+        if(not self.isBuild):
+            self.buildMaxHeap()
+        self.heap_size+=1
+        if(self.heap_size<=len(self.A)):
+            self.A[self.heap_size-1]=self.sentinel_minus
+        else:
+            self.A.append(self.sentinel_minus)
+            self.length=len(self.A)
+        self.heapIncreaseKey(self.heap_size-1,key)
+
+        
+
+
+
+
+
+
 
 
 
 if __name__=='__main__':
-    A=[16,14,10,8,7,9,3,2,4,1]
-    heap_sort=HEAPSORT(A)
-    heap_sort.heapSort()
-    print heap_sort.A
-    print A
+    A=[16,14,10,8,7,9,3,2,100,4,1]
+    max_heap=MAXHEAP(A)
+    #heap_sort.heapSort()
+    max_heap.buildMaxHeap()
+    print max_heap.heapExtractMax()
+    
+
